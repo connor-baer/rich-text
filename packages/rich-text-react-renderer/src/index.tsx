a108a92e2041ce4fc7a52bff6d9003256c49f771
Mike Molisani <mgmolisani@gmail.com>
2019-02-06 14:38:16 -0500

    refactor: 💡 Pulled nested functions out of main

--- packages/rich-text-react-renderer/src/index.tsx
+++ packages/rich-text-react-renderer/src/index.tsx
@@ -78,40 +78,41 @@ export function documentToReactComponents(
   richTextDocument: Document,
   options: Options = {},
 ): ReactNode {
-  const renderNode = {
-    ...defaultNodeRenderers,
-    ...options.renderNode,
-  };
-  const renderMark = {
-    ...defaultMarkRenderers,
-    ...options.renderMark,
-  };
+  return nodeListToReactComponents(richTextDocument.content, {
+    renderNode: {
+      ...defaultNodeRenderers,
+      ...options.renderNode,
+    },
+    renderMark: {
+      ...defaultMarkRenderers,
+      ...options.renderMark,
+    },
+  });
+}
 
-  function convertNodeList(nodes: CommonNode[]): ReactNode {
-    return nodes.map(
-      (node: CommonNode, index: number): ReactNode => {
-        return appendKeyToValidElement(convertNode(node), index);
-      },
-    );
-  }
+function nodeListToReactComponents(nodes: CommonNode[], options: Options): ReactNode {
+  return nodes.map(
+    (node: CommonNode, index: number): ReactNode => {
+      return appendKeyToValidElement(nodeToReactComponent(node, options), index);
+    },
+  );
+}
 
-  function convertNode(node: CommonNode): ReactNode {
-    if (helpers.isText(node)) {
-      return node.marks.reduce((value: ReactNode, mark: Mark): ReactNode => {
-        if (!renderMark[mark.type]) {
-          return value;
-        }
-        return renderMark[mark.type](value);
-      }, node.value);
-    } else {
-      if (!node.nodeType || !renderNode[node.nodeType]) {
-        return null;
+function nodeToReactComponent(node: CommonNode, options: Options): ReactNode {
+  const { renderNode, renderMark } = options;
+  if (helpers.isText(node)) {
+    return node.marks.reduce((value: ReactNode, mark: Mark): ReactNode => {
+      if (!renderMark[mark.type]) {
+        return value;
       }
-      return renderNode[node.nodeType](node, convertNodeList(node.content));
+      return renderMark[mark.type](value);
+    }, node.value);
+  } else {
+    if (!node.nodeType || !renderNode[node.nodeType]) {
+      return null;
     }
+    return renderNode[node.nodeType](node, nodeListToReactComponents(node.content, options));
   }
-
-  return convertNodeList(richTextDocument.content);
 }
 
 function appendKeyToValidElement(element: ReactNode, key: number): ReactNode {
